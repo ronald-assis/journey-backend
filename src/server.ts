44d4dc72e021512376a07cafcd376846e838348f
ronald.assis <ronaldassis71@gmail.com>
2024-07-10 12:53:40 -0300

    feat(email): create valited of email

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import {serializerCompiler, validatorCompiler} from "fastify-type-provider-zod";
 import {createTrip} from "./routes/create-trip";
+import {confirmTrip} from "./routes/confirm-trip";
 
 const app = fastify()
 const PORT = 3333
@@ -9,6 +10,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip, {prefix: '/api'})
+app.register(confirmTrip, {prefix: '/api'})
 
 app.listen({ port: PORT }).then(() => {
     console.log(`Server running in port: ${PORT}`)
